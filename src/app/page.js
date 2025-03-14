5776d197bc070fd425742b488c2cfc4ac80cb024
Renan Cavichi <renancavichi@gmail.com>
2025-03-13 22:02:38 -0300

    exemplo useEffect e CSS in JS

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,25 @@
 'use client'
 
-import { useState } from "react";
+import Footer from "@/components/Footer";
+import { useState, useEffect } from "react";
 
 export default function Home() {
 
   const [chat, setChat] = useState([])
-  const [message, setMessage] = useState('')
+  const [message, setMessage] = useState('ola')
+
+  useEffect(() => {
+    const getMessages = async () => {
+      const response = await fetch('http://localhost:3000/messages')
+      const data = await response.json()
+      setChat(data)
+    }
+    getMessages()
+  },[])
+
+  useEffect(() => {
+    console.log(message)
+  },[message])
 
   const handleSubmit = async () => {
     const response = await fetch('http://localhost:3000/ia', {
@@ -32,8 +46,21 @@ export default function Home() {
       
       <div className="fixed bottom-6 mx-auto bg-zinc-700 flex items-center justify-center p-4 rounded-lg text-white text-center gap-2">
         <textarea value={message} onChange={(e) => setMessage(e.target.value)} className="w-[600px] border border-zinc-900 rounded p-2 resize-none"></textarea>
-        <button onClick={handleSubmit}>Enviar</button>
+        <button style={styles.button} onClick={handleSubmit}>Enviar</button>
       </div>
+
+      <Footer />
     </div>
   );
+}
+
+const styles = {
+  button: {
+    backgroundColor: '#456456',
+    border: '1px solid #CCC',
+    borderRadius: '5px',
+    color: '#FFF',
+    padding: '5px 10px',
+    cursor: 'pointer'
+  }
 }
